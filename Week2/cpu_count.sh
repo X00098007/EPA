2d19a035b126cf8faeb509fc320c09e6383e5193
epauser <epauser@localhost.localdomain>
2018-10-03 21:23:24 +0100

    cpu count

--- Week2/cpu_count.sh
+++ Week2/cpu_count.sh
@@ -6,6 +6,8 @@
 # grep is used to search for a string
 # pipe is used to join output of one command as input to another
 
-echo "The number of cores is: "
-grep rocessor /proc/cpuinfo | wc -l 
+# `` executes the command and we store the result in cnt as a variable
+
+cnt=`grep rocessor /proc/cpuinfo | wc -l`
+echo "The number of cores is: " $cnt
 
